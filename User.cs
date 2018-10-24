ff0a3d69e1efb96e83b27e581974423e6faa3d4b
ugesh <ugesh.roopchand@hotmail.com>
2018-10-24 13:33:20 +0400

    test

---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace LHBOL
+{
+
+    [Table("User")]
+   public class User
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public string Password { get; set; }
+        //1 User can submit many urls
+        public IEnumerable<LHUrl> LHUrls { get; set; }
+    }
+}
